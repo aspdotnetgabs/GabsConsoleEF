16194adc38de6be2d6d28eb2ec6665ecfa714362
hewbertgabon@gmail.com <hewbertgabon@gmail.com>
2018-12-08 04:09:50 +0800

    Init commit

---
+++ GabsConsoleEF/MyDbContext.cs
@@ -0,0 +1,20 @@
+﻿using GabsConsoleEF.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GabsConsoleEF
+{
+    class MyDbContext : DbContext
+    {
+        public MyDbContext():base("DbJetAccess")
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
